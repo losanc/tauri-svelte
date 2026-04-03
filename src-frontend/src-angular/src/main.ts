cc2d901a6968362df50ee5478a24287ec30c3927
cgtinker <d.hsu@posteo.de>
2026-04-04 00:16:40 +0200

    rm test frontends

--- src-frontend/src-angular/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser'
-import { appConfig } from './app/app.config'
-import { App } from './app/app'
-import { Popout } from './app/popout'
-
-const isPopout = window.location.pathname.startsWith('/popout')
-const rootComponent = isPopout ? Popout : App
-
-bootstrapApplication(rootComponent, appConfig).catch((err) => console.error(err))
